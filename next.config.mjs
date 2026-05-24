7f467216528b9b34473922b12756f0a4d9f91d06
teuszqyyy <teussete2409@gmail.com>
2026-05-24 19:08:00 -0300

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '50mb',
+    },
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  },
+}
+
+export default nextConfig
